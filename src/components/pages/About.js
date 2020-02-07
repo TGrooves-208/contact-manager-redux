626f2ba21e30ea79de8b05fe03ea857e82d64446
redeyedev-208 <redeyedev.208@gmail.com>
2020-02-06 20:38:36 -0700

    created build file

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">About Contact Manager</h1>
+      <p className="lead">
+        Contact Manager Application. Using React, Redux-Thunk and JSON
+        Placeholder to mimic the behavior of a backend.
+      </p>
+      <p className="text-secondary">Version 1.0.0</p>
+    </div>
+  );
+};
